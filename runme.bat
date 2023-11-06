f5bad49f84aaccf1c60b77b7c8879d4aa9327b41
Draco <siamak.h.a.z@gmail.com>
2023-11-06 00:21:53 -0500

    add python and jupiter extentions

--- runme.bat
+++ runme.bat
@@ -2,4 +2,5 @@ icacls "..\vscode\" /grant Everyone:F /t
 
 mkdir .\data\project
 docker compose up -d 
+start http://localhost:1988
 docker stats 
--- runme.sh
+++ runme.sh
@@ -1,3 +1,4 @@
 mkdir /data/project
 docker compose up -d 
+open http://localhost:1988
 docker stats
